24ac71bf922ffa07422cd9cf9cfc6b1515a951c7
unknown <brcampbell001@outlook.com>
2018-05-12 11:00:08 -0500

    Blog Application Completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const FETCH_POSTS = "fetch_posts";
 export const CREATE_POSTS = "create_post";
-export const FETCH_POST = 'fetch_post';
+export const FETCH_POST = "fetch_post";
+export const DELETE_POST = 'delete_post';
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=emotional12345";
@@ -33,5 +34,16 @@ export function fetchPost(id) {
   return {
     type: FETCH_POST,
     payload: request
+  };
+}
+
+export function deletePost(id, cb) {
+  const request = axios
+    .delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    .then(() => cb());
+
+  return {
+    type: DELETE_POST,
+    payload: id
   };
 }
